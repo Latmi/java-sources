f2c098bad9e4b339f09de5ca32eb8dc21cbca351
Latmi <sbezuglii@mail.ru>
2017-08-09 15:04:02 +0300

    try adding Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,32 @@
 public class Calculate {
     public static void main(String[] args) {
         System.out.println("Calculate...");
+        Calculate calc = new Calculate();
+        int sum1 = Integer.valueOf(args[0]);
+        int first = Integer.valueOf(args[1]);
+        int second = Integer.valueOf(args[2]);
+        System.out.println(first + calc.job(sum1) + second + " = " + calc.sum(first, second, sum1));
+    }
+
+    public int sum(int first, int second, int sum1) {
+        int sum = 0;
+        if (sum1 == 1) {
+            sum = first + second;
+        }
+        if (sum1 == 2)  {
+            sum = first * second;
+        }
+        return sum;
+    }
+
+    public String job(int sum1) {
+        String job = "";
+        if (sum1 == 1) {
+            job = " + ";
+        }
+        if (sum1 == 2) {
+            job = " * ";
+        }
+        return job;
     }
 }
